4033f37dcc79786ab014d17264ceacb8106dd77a
Gab102323 <gamboadavidgabriel@gmail.com>
2024-07-02 03:43:19 +0800

    added!

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('forgiveBtn').addEventListener('click', function() {
+    alert('Thank you for forgiving me. I love you! and i always will^^');
+    // You can add more actions here if needed, like redirecting to another page or showing more content
+});
